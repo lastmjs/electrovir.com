220149ba694d8712943dd1352e2cadb8949d9b1c
electrovir <electrovir@users.noreply.github.com>
2019-03-07 18:52:42 -0700

    Add ElectrovirApp as custom element

--- elements/electrovir-app.ts
+++ elements/electrovir-app.ts
@@ -1,1 +1,14 @@
-// nothing here yet
+class ElectrovirApp extends HTMLElement {
+    connectedCallback() {
+        console.log('The app has been connected!');
+    }
+
+    render() {
+        return html`
+            yo I heard you like custom elements
+        `;
+    }
+
+}
+
+window.customElements.define('electrovir-app', ElectrovirApp);
